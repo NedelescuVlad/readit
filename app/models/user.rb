366ad41827b82114753cbb69d15a6f0644828fcf
NedelescuVlad <nedelescu.vlad@gmail.com>
2016-12-25 19:54:28 +0000

    Create initial user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+	VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+	validates :name, presence: true
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+	has_secure_password
+
+	validates :password, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,53 @@
 require 'rails_helper'
 
-describe User , :type => :model do 
-	it "asserts the truth" do
-		user = build(:user)
-		expect(true).to eq true
+RSpec.describe User, type: :model do
+
+	let(:user) { build(:user) }
+
+	context "with valid fields" do
+		it "is valid" do
+			expect(user.valid?).to eq true
+		end
+	end
+
+	context "with blank name" do
+		it "is invalid" do
+			user.name = " "
+			expect(user.valid?).to eq false
+		end
+	end
+
+	context "with blank email" do
+		it "is invalid" do
+			user.email = " "
+			expect(user.valid?).to eq false
+		end
+	end
+
+	context "with blank password" do
+		it "is invalid" do
+			user.password = " "
+			expect(user.valid?).to eq false
+		end
+	end
+
+	context "with email address of the wrong format" do
+		it "is invalid" do
+			invalid_emails = %w[kappa kappa@kappa @kappa.com vlad@kappa..com @]
+			invalid_emails.each do |invalid_email|
+				user.email = invalid_email 
+				expect(user.valid?).to eq false
+			end
+		end
+	end
+
+	context "with email address of the right format" do
+		it "is valid" do
+			valid_emails = %w[vlad@example.com vlad.nedelescu@example.com vlad@kcl.ac.uk vlad.nedelescu@kcl.ac.uk]
+			valid_emails.each do |valid_email|
+				user.email = valid_email
+				expect(user.valid?).to eq true
+			end
+		end
 	end
 end
