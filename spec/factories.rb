cb907760a9501b5bd78c38fc5276407a4b43a241
k1502691 <vlad.nedelescu@kcl.ac.uk>
2016-12-25 18:39:29 +0000

    Get factory girl to work

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-#FactoryGirl.define do 
-	#factory :user do
-		#name "Vlad Nedelescu"
-		#email "nedelescu.vlad@gmail.com"
-		#password "itsasecret"
-		#password_confirmation "itsasecret"
-	#end
-#end
+FactoryGirl.define do 
+	factory :user do
+		name "Vlad Nedelescu"
+		email "nedelescu.vlad@gmail.com"
+		password "itsasecret"
+		password_confirmation "itsasecret"
+	end
+end
